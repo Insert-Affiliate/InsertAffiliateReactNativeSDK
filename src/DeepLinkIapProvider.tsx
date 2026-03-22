903216c03c39309c3e7a98850d47a951af91add9
micahel <michaelbutlerjersey@gmail.com>
2026-03-22 16:59:54 +0000

    Debug: console.log for system info flag

--- src/DeepLinkIapProvider.tsx
+++ src/DeepLinkIapProvider.tsx
@@ -209,7 +209,7 @@ const DeepLinkIapProvider: React.FC<T_DEEPLINK_IAP_PROVIDER> = ({
 
     if (insertLinksEnabledParam && Platform.OS === 'ios') {
       const systemInfoSent = await getValueFromAsync(ASYNC_KEYS.SYSTEM_INFO_SENT);
-      verboseLog(`System info sent flag: ${systemInfoSent ? 'true (skipping)' : 'false (will send)'}`);
+      console.log(`[Insert Affiliate] System info sent flag: "${systemInfoSent}" (type: ${typeof systemInfoSent})`);
       if (!systemInfoSent) {
         // Set flag immediately to prevent concurrent init calls from sending twice
         await saveValueInAsync(ASYNC_KEYS.SYSTEM_INFO_SENT, 'pending');
